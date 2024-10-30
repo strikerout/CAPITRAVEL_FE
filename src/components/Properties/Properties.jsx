1f5f00c3663ad5d1163acd95489fe8701db75657
Chiara <chiarapozzig@gmail.com>
2024-10-30 00:30:41 -0300

    add form validations for add/edit property y category

--- src/components/Properties/Properties.jsx
+++ src/components/Properties/Properties.jsx
@@ -1,70 +1,81 @@
 import React, { useState } from 'react';
-import useProperties from '../../hooks/useProperties'
-import PrimaryButton from '../Buttons/PrimaryButton'
-
+import useProperties from '../../hooks/useProperties';
+import PrimaryButton from '../Buttons/PrimaryButton';
 
 const Properties = () => {
-const {fetchProperties, fetchPropertyByID, properties, loading, error, addProperty, updateExistingProperty, removeProperty  } = useProperties();
+  const { fetchProperties, fetchPropertyByID, properties, loading, error, addProperty, updateExistingProperty, removeProperty } = useProperties();
 
-const [newProperty, setNewProperty] =  useState({ name: '', description: '', image: '' });
-const [idToEdit, setIdToEdit] = useState('');
+  const [newProperty, setNewProperty] = useState({ name: '', description: '', image: '' });
+  const [idToEdit, setIdToEdit] = useState('');
+  const [errors, setErrors] = useState({ name: '', description: '' });
 
-
-const handleAddProperty = () => {
-     addProperty(newProperty);
-    console.log(newProperty, 'NEW PROPERTY')
-    setNewProperty({ name: '', description: '', image: '' }); // Limpiar el formulario
+  const handleAddProperty = () => {
+    addProperty(newProperty);
+    console.log(newProperty, 'NEW PROPERTY');
+    setNewProperty({ name: '', description: '', image: '' });
   };
 
   const handleRemoveProperty = (id) => {
     const confirm = window.confirm("Sure to delete this property?");
-    if(confirm){
-        removeProperty(id); 
+    if (confirm) {
+      removeProperty(id);
       console.log("Elemento eliminado");
-    }else{
-      console.log("Cancelado")
+    } else {
+      console.log("Cancelado");
     }
   };
 
-  const enableEditMode = async (id) =>{
-    const toEdit = await fetchPropertyByID(id) 
-    setNewProperty({ name: toEdit.name, description: toEdit.description, image: toEdit.image})
-    setIdToEdit(id)
-  }
+  const enableEditMode = async (id) => {
+    const toEdit = await fetchPropertyByID(id);
+    setNewProperty({ name: toEdit.name, description: toEdit.description, image: toEdit.image });
+    setIdToEdit(id);
+  };
 
   const handleEditProperty = () => {
     const confirm = window.confirm("Sure to edit this property?");
-    if(confirm){
-    updateExistingProperty(idToEdit, newProperty)
-    setNewProperty({ name: '', description: '', image: '' }); // Limpiar el formulario
-    setIdToEdit('')
-    console.log("Elemento editado");
-    }else{
-      console.log("Cancelado")
+    if (confirm) {
+      updateExistingProperty(idToEdit, newProperty);
+      setNewProperty({ name: '', description: '', image: '' });
+      setIdToEdit('');
+      console.log("Elemento editado");
+    } else {
+      console.log("Cancelado");
     }
-  }
+  };
 
   const cancelEdit = () => {
-    setIdToEdit('')
-    setNewCategory({ name: '', description: '', image: '' }); // Limpiar el formulario
-  }
+    setIdToEdit('');
+    setNewProperty({ name: '', description: '', image: '' });
+  };
 
-  const handleSubmit = (e) =>{
+  const handleSubmit = (e) => {
     e.preventDefault();
+    let validationErrors = { name: '', description: '' };
 
-    idToEdit? handleEditProperty() : handleAddProperty();
-  }
+    if (newProperty.name.length < 3 || newProperty.name.length > 32) {
+      validationErrors.name = 'Name must be between 3 and 32 characters.';
+    }
+    if (newProperty.description.length < 15 || newProperty.description.length > 256) {
+      validationErrors.description = 'Description must be between 15 and 256 characters.';
+    }
+
+    if (validationErrors.name || validationErrors.description) {
+      setErrors(validationErrors);
+    } else {
+      setErrors({ name: '', description: '' });
+      idToEdit ? handleEditProperty() : handleAddProperty();
+    }
+  };
 
   if (loading) return <div>Loading...</div>;
   if (error) return <div>Error: {error.message}</div>;
-  
 
   return (
     <>
       <h3>Properties</h3>
       <form className='adminForm' onSubmit={handleSubmit}>
         <div>
-          <label for='name'>Name</label>
+          <label htmlFor='name'>Name</label>
           <input
             type="text"
             placeholder="Enter a name"
@@ -73,10 +84,11 @@ const handleAddProperty = () => {
             onChange={(e) => setNewProperty({ ...newProperty, name: e.target.value })}
             required
           />
+          {errors.name && <p className="error">{errors.name}</p>}
         </div>
 
         <div>
-          <label for='description'>Description</label>
+          <label htmlFor='description'>Description</label>
           <input
             type="text"
             placeholder="Enter a description"
@@ -85,10 +97,11 @@ const handleAddProperty = () => {
             onChange={(e) => setNewProperty({ ...newProperty, description: e.target.value })}
             required
           />
+          {errors.description && <p className="error">{errors.description}</p>}
         </div>
-        
+
         <div>
-          <label for='image'>Add a icon</label>
+          <label htmlFor='image'>Add a icon</label>
           <input
             type="text"
             placeholder="Image"
@@ -99,23 +112,21 @@ const handleAddProperty = () => {
           />
           <p>Supported files .PNG .SVG</p>
         </div>
-        
+
         {idToEdit ? 
           <div className='buttonsContainer'>
             <PrimaryButton type='submit'>Save Property</PrimaryButton>
             <PrimaryButton func={cancelEdit}>Cancel</PrimaryButton>
           </div>
-        : 
-        <PrimaryButton type='submit'>Add Property</PrimaryButton>}
+          : 
+          <PrimaryButton type='submit'>Add Property</PrimaryButton>}
       </form>
-      
 
       <div className='adminList'>
         <div className='headerList'>
           <h4>ID</h4>
           <h4>Name</h4>
           <h4>Description</h4>
-  
         </div>
         <ul className='bodyList'>
           {properties.map((property) => (
@@ -125,7 +136,7 @@ const handleAddProperty = () => {
               <p>{property.description}</p>
 
               <div>
-                <svg onClick={() => enableEditMode(property.id)} width="1em" height="1em"xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><g fill="none"><path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m5 16l-1 4l4-1L19.586 7.414a2 2 0 0 0 0-2.828l-.172-.172a2 2 0 0 0-2.828 0z"/><path fill="currentColor" d="m5 16l-1 4l4-1L18 9l-3-3z"/><path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m15 6l3 3m-5 11h8"/></g></svg>
+                <svg onClick={() => enableEditMode(property.id)} width="1em" height="1em" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><g fill="none"><path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m5 16l-1 4l4-1L19.586 7.414a2 2 0 0 0 0-2.828l-.172-.172a2 2 0 0 0-2.828 0z"/><path fill="currentColor" d="m5 16l-1 4l4-1L18 9l-3-3z"/><path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m15 6l3 3m-5 11h8"/></g></svg>
 
                 <svg onClick={() => handleRemoveProperty(property.id)} xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 12 12"><path fill="currentColor" d="M5 3h2a1 1 0 0 0-2 0M4 3a2 2 0 1 1 4 0h2.5a.5.5 0 0 1 0 1h-.441l-.443 5.17A2 2 0 0 1 7.623 11H4.377a2 2 0 0 1-1.993-1.83L1.941 4H1.5a.5.5 0 0 1 0-1zm3.5 3a.5.5 0 0 0-1 0v2a.5.5 0 0 0 1 0zM5 5.5a.5.5 0 0 0-.5.5v2a.5.5 0 0 0 1 0V6a.5.5 0 0 0-.5-.5"/></svg>
 
@@ -133,10 +144,10 @@ const handleAddProperty = () => {
             </li>
           ))}
         </ul>
-      </div> 
+      </div>
 
     </>
   )
 }
 
-export default Properties
+export default Properties;
