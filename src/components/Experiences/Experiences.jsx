b0b824ee5401083ff1427ada9243359de74652cb
LinaAlvarado <linamariaalvaradopallares@gmail.com>
2024-10-30 17:19:00 -0500

    form create an experience and can add images

--- src/components/Experiences/Experiences.jsx
+++ src/components/Experiences/Experiences.jsx
@@ -6,16 +6,7 @@ import ImageUploader from "../ImageUploader";
 import PrimaryButton from "../Buttons/PrimaryButton";
 
 const Experiences = () => {
-  const {
-    fetchExperiences,
-    fetchExperienceByID,
-    addExperience,
-    experiences,
-    loading,
-    error,
-    updateExistingExperience,
-    removeExperience,
-  } = useExperiences();
+  const { addExperience } = useExperiences();
 
   const { properties } = useProperties();
   const { categories } = useCategories();
@@ -26,7 +17,6 @@ const Experiences = () => {
   const [selectedProperty, setSelectedProperty] = useState("");
   const [selectedProperties, setSelectedProperties] = useState([]);
 
-
   const [newExperience, setNewExperience] = useState({
     title: "",
     country: "",
@@ -34,68 +24,16 @@ const Experiences = () => {
     description: "",
     duration: "",
     images: [],
-    categories: [],
-    properties: [],
+    categoryIds: [],
+    propertyIds: [],
   });
 
-    // Manejar la adición de imágenes
-    const handleImagesAdded = (base64Images) => {
-      setNewExperience((prevExperience) => ({
-        ...prevExperience,
-        images: [...prevExperience.images, ...base64Images], // Añadir nuevas imágenes a la experiencia
-      }));
-    };
-
-   
-
-  const handleAddExperience = (e) => {
-    e.preventDefault();
-    // addExperience(newExperience);
-    console.log(newExperience)
-    setNewExperience({
-      title: "",
-      country: "",
-      ubication: "",
-      description: "",
-      duration: "",
-      images: [],
-      categories: [],
-      properties: [],
-    });
+  const handleImagesAdded = (base64Images) => {
+    setNewExperience((prevExperience) => ({
+      ...prevExperience,
+      images: [...prevExperience.images, ...base64Images], // Añadir nuevas imágenes a la experiencia
+    }));
   };
-
-  const handleRemoveExperience = (id) => {
-    const confirm = window.confirm("Sure to delete this experience?");
-    if (confirm) {
-      removeExperience(id);
-      console.log("Elemento eliminado");
-    } else {
-      console.log("Cancelado");
-    }
-  };
-
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setExperienceData({
-      ...experienceData,
-      [name]: value,
-    });
-  };
-
-//   const handleCategoryChange = (selectedCategory) => {
-//     setExperienceData({
-//       ...experienceData,
-//       category: selectedCategory,
-//     });
-//   };
-
-//   const handlePropertyChange = (selectedProperty) => {
-//     setExperienceData({
-//       ...experienceData,
-//       properties: selectedProperty,
-//     });
-//   };
-
 
   const handleSelectChange = (event) => {
     setSelectedCategory(event.target.value);
@@ -112,11 +50,11 @@ const Experiences = () => {
     ) {
       const updatedCategories = [...selectedCategories, categoryToAdd];
       setSelectedCategories(updatedCategories);
-      setNewExperience({
-        ...newExperience,
-        categories: updatedCategories,
-      });
-      setSelectedCategory(""); 
+      setNewExperience((prevExperience) => ({
+        ...prevExperience,
+        categoryIds: [...prevExperience.categoryIds, categoryToAdd.id], // Solo guarda el ID
+      }));
+      setSelectedCategory([]);
     }
   };
 
@@ -124,12 +62,10 @@ const Experiences = () => {
     const updatedCategories = selectedCategories.filter(
       (category) => category.id !== id
     );
-
     setSelectedCategories(updatedCategories);
-
     setNewExperience({
       ...newExperience,
-      categories: updatedCategories,
+      categoryIds: updatedCategories,
     });
   };
 
@@ -137,217 +73,300 @@ const Experiences = () => {
     setSelectedProperty(event.target.value);
   };
 
-  const handleAddProperty = (e) => {
+   const handleAddProperty = (e) => {
+     e.preventDefault();
+     const propertyToAdd = properties.find(
+       (property) => property.id === Number(selectedProperty)
+     );
+     if (
+       propertyToAdd &&
+       !selectedProperties.some((property) => property.id === propertyToAdd.id)
+     ) {
+       const updatedProperties = [...selectedProperties, propertyToAdd];
+       setSelectedProperties(updatedProperties);
+       setNewExperience((prevExperience) => ({
+         ...prevExperience,
+         propertyIds: [...prevExperience.propertyIds, propertyToAdd.id],  //Solo guarda el ID
+       }));
+       setSelectedProperty("");
+     }
+   };
+
+   const handleRemoveProperty = (id) => {
+     const updatedProperties = selectedProperties.filter(
+       (properties) => properties.id !== id
+     );
+
+     setSelectedProperties(updatedProperties);
+
+     setNewExperience({
+       ...newExperience,
+       propertyIds: updatedProperties,
+     });
+   };
+   
+  const handleAddExperience = (e) => {
     e.preventDefault();
-    const propertyToAdd = properties.find(
-        (property) => property.id === Number(selectedProperty)
-    );
-    if (
-        propertyToAdd &&
-        !selectedProperties.some((property) => property.id === propertyToAdd.id) 
-    ) {
-        const updatedProperties = [...selectedProperties, propertyToAdd];
-        setSelectedProperties(updatedProperties);
-        setNewExperience({
-            ...newExperience,
-            properties: updatedProperties,
-        });
-        setSelectedProperty(""); 
-    }
-};
+    addExperience({
+      ...newExperience,
+      categoryIds: newExperience.categoryIds, //Mantiene solo los IDs
+      propertyIds: newExperience.propertyIds,//Mantiene solo los IDs
+    });
 
-  const handleRemoveProperty = (id) => {
-    const updatedProperties = selectedProperties.filter(
-      (properties) => properties.id !== id
-    );
+    console.log(newExperience);
 
-    setSelectedProperties(updatedProperties);
+    setSelectedCategories([]);
+    setSelectedProperties([]);
 
     setNewExperience({
-      ...newExperience,
-      properties: updatedProperties,
+      title: "",
+      country: "",
+      ubication: "",
+      description: "",
+      duration: "",
+      images: [],
+      categoryIds: [],
+      propertyIds: [],
     });
   };
 
-  const handleSubmit = (e) => {
-    e.preventDefault;
-    handleAddExperience();
-   
-  };
+const handleSubmit =(e)=>{
+  e.preventDefault();
+  handleAddExperience(e)
+}
 
+  // const handleRemoveExperience = (id) => {
+  //   const confirm = window.confirm("Sure to delete this experience?");
+  //   if (confirm) {
+  //     removeExperience(id);
+  //     console.log("Elemento eliminado");
+  //   } else {
+  //     console.log("Cancelado");
+  //   }
+  // };
 
-  return (
-    <>
-      <h3>Experiences</h3>
-      <section>
-        <form className="adminForm" onSubmit={handleSubmit}>
-          <div>
-            <h5>About the Experience</h5>
-            <div>
-              <label for="title">Title</label>
-              <input
-                type="text"
-                placeholder="Enter a title"
-                id="title"
-                value={newExperience.title}
-                onChange={(e) =>
-                  setNewExperience({ ...newExperience, title: e.target.value })
-                }
-                required
-              />
-            </div>
+  // const handleChange = (e) => {
+  //   const { name, value } = e.target;
+  //   setExperienceData({
+  //     ...experienceData,
+  //     [name]: value,
+  //   });
+  // };
 
-            <div>
-              <label for="description"> Description</label>
-              <p>0/500</p>
-              <textarea 
-              name="description" 
-              placeholder="Enter a description" 
-              maxLength={500}
-              value={newExperience.description}
-              onChange={(e) => 
-                setNewExperience({ 
-                  ...newExperience, 
-                  description: e.target.value 
-                })
-              } 
-              required
-              
-              />
-            </div>
-          </div>
+  //   const handleCategoryChange = (selectedCategory) => {
+  //     setExperienceData({
+  //       ...experienceData,
+  //       category: selectedCategory,
+  //     });
+  //   };
 
-          <div>
-            <h5>Where is it?</h5>
-            <div>
-              <label for="country">Country</label>
-              <input
-                type="text"
-                placeholder="Enter country name"
-                id="country"
-                value={newExperience.country}
-                onChange={(e) =>
-                  setNewExperience({
-                    ...newExperience,
-                    country: e.target.value,
-                  })
-                }
-                required
-              />
-            </div>
-            <div>
-              <label for="ubication">ubication</label>
-              <input
-                type="text"
-                placeholder="City, state/region"
-                id="ubication"
-                value={newExperience.ubication}
-                onChange={(e) =>
-                  setNewExperience({
-                    ...newExperience,
-                    ubication: e.target.value,
-                  })
-                }
-                required
-              />
-            </div>
-          </div>
+  //   const handlePropertyChange = (selectedProperty) => {
+  //     setExperienceData({
+  //       ...experienceData,
+  //       properties: selectedProperty,
+  //     });
+  //   };
 
-          <div>
-            <h5>Specifications</h5>
-            <div>
-              <label for="categorySelect">Category</label>
-              <select
-                id="categorySelect"
-                value={selectedCategory}
-                onChange={handleSelectChange}
-              >
-                <option value="" disabled>
-                Select Categories
-                </option>
-                {categories.map((category) => (
-                  <option key={category.id} value={category.id}>
-                    {category.name}
-                  </option>
-                ))}
-              </select>
-              <button type="button" onClick={handleAddCategory}>Add Category</button>
+     return (
+       <>
+         <h3>Experiences</h3>
+         <section>
+           <form className="adminForm" onSubmit={handleSubmit}>
+             <div>
+               <h5>About the Experience</h5>
+               <div>
+                 <label for="title">Title</label>
+                 <input
+                   type="text"
+                   placeholder="Enter a title"
+                   id="title"
+                   value={newExperience.title}
+                   onChange={(e) =>
+                     setNewExperience({ ...newExperience, title: e.target.value })
+                   }
+                   required
+                 />
+               </div>
 
-              <h3>Selected Categories</h3>
-              <ul>
-                {selectedCategories.map((category) => (
-                  <li key={category.id}>
-                    {category.name}
-                    <button type="button" onClick={() => handleRemoveCategory(category.id)}>
-                      Delete
-                    </button>
-                  </li>
-                ))}
-              </ul>
-            </div>
+               <div>
+                 <label for="description"> Description</label>
+                 <p>0/500</p>
+                 <textarea
+                   name="description"
+                   placeholder="Enter a description"
+                   maxLength={500}
+                   value={newExperience.description}
+                   onChange={(e) =>
+                     setNewExperience({
+                       ...newExperience,
+                       description: e.target.value,
+                     })
+                   }
+                   required
+                 />
+               </div>
+             </div>
 
-            <div>
-              <label for="propertySelect">Property</label>
-              <select
-                id="propertySelect"
-                value={selectedProperty}
-                onChange={handleSelectChangeProperty}
-              >
-                <option value="" disabled>
-                Select Properties
-                </option>
-                {properties.map((property) => (
-                  <option key={property.id} value={property.id}>
-                    {property.name}
-                  </option>
-                ))}
-              </select>
-              <button type="button" onClick={handleAddProperty}>Add Property</button>
+             <div>
+               <h5>Where is it?</h5>
+               <div>
+                 <label for="country">Country</label>
+                 <input
+                   type="text"
+                   placeholder="Enter country name"
+                   id="country"
+                   value={newExperience.country}
+                   onChange={(e) =>
+                     setNewExperience({
+                       ...newExperience,
+                       country: e.target.value,
+                     })
+                   }
+                   required
+                 />
+               </div>
+               <div>
+                 <label for="ubication">ubication</label>
+                 <input
+                   type="text"
+                   placeholder="City, state/region"
+                   id="ubication"
+                   value={newExperience.ubication}
+                   onChange={(e) =>
+                     setNewExperience({
+                       ...newExperience,
+                       ubication: e.target.value,
+                     })
+                   }
+                   required
+                 />
+               </div>
+             </div>
 
-              <h3>Selected Properties:</h3>
-              <ul>
-                {selectedProperties.map((property) => (
-                  <li key={property.id}>
-                    {property.name}
-                    <button type="button" onClick={() => handleRemoveProperty(property.id)}>
-                      Delete
-                    </button>
-                  </li>
-                ))}
-              </ul>
-            </div>
-          </div>
+             <div>
+               <h5>Specifications</h5>
+               <div>
+                 <label for="categorySelect">Category</label>
+                 <select
+                   id="categorySelect"
+                   value={selectedCategory}
+                   onChange={handleSelectChange}
+                 >
+                   <option value="" disabled>
+                     Select Categories
+                   </option>
+                   {categories.map((category) => (
+                     <option key={category.id} value={category.id}>
+                       {category.name}
+                     </option>
+                   ))}
+                 </select>
+                 <button type="button" onClick={handleAddCategory}>
+                   Add Category
+                 </button>
 
-          <div>
-            <h5>Duration</h5>
-            {/* select duration */}
-          </div>
+                 <h3>Selected Categories</h3>
+                 <ul>
+                   {selectedCategories.map((category) => (
+                     <li key={category.id}>
+                       {category.name}
+                       <button
+                         type="button"
+                         onClick={() => handleRemoveCategory(category.id)}
+                       >
+                         Delete
+                       </button>
+                     </li>
+                   ))}
+                 </ul>
+               </div>
 
-          <div>
-            <h5>Add Images</h5>
-            <div>
-            <h5>Add Images</h5>
-            <ImageUploader onImagesAdded={handleImagesAdded} /> {/* Integrar ImageUploader */}
-            <div>
-              <h6>Imágenes Cargadas:</h6>
-              <ul>
-                {newExperience.images.map((image, index) => (
-                  <li key={index}>
-                    <img src={image} alt={`Imagen ${index + 1}`} style={{ width: '100px', height: 'auto' }} />
-                  </li>
-                ))}
-              </ul>
-            </div>
-          </div>
-          </div>
-         <PrimaryButton type="submit">Add Experience</PrimaryButton>
-        </form>
-        <div>
-          <div>Titulo de la tabla</div>
-          <ul></ul>
-        </div>
-      </section>
-    </>
-  );
-};
-export default Experiences;
+               <div>
+                 <label for="propertySelect">Property</label>
+                 <select
+                   id="propertySelect"
+                   value={selectedProperty}
+                   onChange={handleSelectChangeProperty}
+                 >
+                   <option value="" disabled>
+                     Select Properties
+                   </option>
+                   {properties.map((property) => (
+                     <option key={property.id} value={property.id}>
+                       {property.name}
+                     </option>
+                   ))}
+                 </select>
+                 <button type="button" onClick={handleAddProperty}>
+                   Add Property
+                 </button>
+
+                 <h3>Selected Properties:</h3>
+                 <ul>
+                   {selectedProperties.map((property) => (
+                     <li key={property.id}>
+                       {property.name}
+                       <button
+                         type="button"
+                         onClick={() => handleRemoveProperty(property.id)}
+                       >
+                         Delete
+                       </button>
+                     </li>
+                   ))}
+                 </ul>
+               </div>
+             </div>
+
+             <div>
+               <h5>Duration</h5>
+               <label for="duration">Time</label>
+               <input
+                 type="number"
+                 placeholder="Enter a Duration"
+                 id="duration"
+                 value={newExperience.duration}
+                 onChange={(e) =>
+                   setNewExperience({ ...newExperience, duration: e.target.value })
+                 }
+                 required
+               />
+             </div>
+
+             <div>
+               <h5>Add Images</h5>
+               <div>
+                 <h5>Add Images</h5>
+                 <ImageUploader onImagesAdded={handleImagesAdded} />{" "}
+                 {/* Integrar ImageUploader */}
+                 <div>
+                   <h6>Imágenes Cargadas:</h6>
+                   <ul>
+                     {newExperience.images.map((image, index) => (
+                       <li key={index}>
+                         <img
+                           src={image}
+                           alt={`Imagen ${index + 1}`}
+                           style={{ width: "100px", height: "auto" }}
+                         />
+                       </li>
+                     ))}
+                   </ul>
+                 </div>
+               </div>
+             </div>
+             <PrimaryButton type="submit" func={handleAddExperience}>
+               Add Experience
+             </PrimaryButton>
+           </form>
+           <div>
+             <div>Titulo de la tabla</div>
+             <ul></ul>
+           </div>
+         </section>
+       </>
+     );
+   };
+   export default Experiences;
+
+  
