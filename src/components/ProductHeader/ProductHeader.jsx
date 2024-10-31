e1c8ae8d2d3e13c3975ec36cdfd818cfd5e6b066
Carlos Colmenares <carlosshmx@hotmail.com>
2024-10-31 14:08:13 -0300

    PFG5-83_fixed_arrow_position

--- src/components/ProductHeader/ProductHeader.jsx
+++ src/components/ProductHeader/ProductHeader.jsx
@@ -8,14 +8,19 @@ const ProductHeader = ({data}) => {
   return (
     <div className={style.productHeader}>
       <div className={style.productTitle}>
-        <svg onClick={()=>navigate(-1)} xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 24 24"><path fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5" d="m14 7l-5 5l5 5"/></svg>
         <h3>{data.title}</h3>
+        <svg onClick={()=>navigate(-1)} xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 24 24"><path fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5" d="m14 7l-5 5l5 5"/></svg>
       </div>
 
       <div>
         <div className={style.productLocation}>
             <svg xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 24 24"><path fill="currentColor" d="M17.657 5.304c-3.124-3.073-8.189-3.073-11.313 0a7.78 7.78 0 0 0 0 11.13L12 21.999l5.657-5.565a7.78 7.78 0 0 0 0-11.13M12 13.499c-.668 0-1.295-.26-1.768-.732a2.503 2.503 0 0 1 0-3.536c.472-.472 1.1-.732 1.768-.732s1.296.26 1.768.732a2.503 2.503 0 0 1 0 3.536c-.472.472-1.1.732-1.768.732"/></svg>
             <p>{data.ubication + ", "+ data.country}</p>
+        </div>
+
+        <div>
+      
+
         </div>
 
         <div className={style.productcategories}>
