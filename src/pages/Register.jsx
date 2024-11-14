b2caef40992125f16de7d24bbc5987379aefe682
Daniel Chacon <dani3l.uebp@gmail.com>
2024-11-14 02:52:52 -0300

    respect to old format

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -13,8 +13,47 @@ export const Register = () => {
     const [emailSent, setEmailSent] = useState(false); // Nuevo estado para controlar el envío del correo
 
     const validateFields = () => {
-        const newErrors = { name: '', lastname: '', email: '', passwordA: '', passwordB: '' };
-        // ... (el código de validación sigue igual)
+        const newErrors = { name: '', lastname: '', email: '', passwordA: '', passwordB: ''};
+
+        const regexName = /^[a-zA-Z\s]+$/;
+
+        if(!newUser.name) newErrors.name = "Name is required";
+        else if(newUser.name.length < 3 || newUser.name.length > 32){
+            newErrors.name = 'Name must be between 3 and 32 characters.';
+        }
+        else if(!regexName.test(newUser.name)){
+            newErrors.name = 'Name can only have letters and spaces';
+        }
+
+
+        if(!newUser.lastname) newErrors.lastname= "Last name is required";
+        else if(newUser.lastname.length < 3 || newUser.lastname.length > 32){
+            newErrors.lastname = 'Last name must be between 3 and 32 characters.';
+        } 
+        else if(!regexName.test(newUser.lastname)){
+            newErrors.lastname = 'Last name can only have letters and spaces';
+        }
+
+        const regexEmail = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+        if(!newUser.email) newErrors.email = "Email is required"
+        else if(!regexEmail.test(newUser.email)){ newErrors.email= "Use a valid email";}
+
+
+        const regexPassword = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+
+        if(!newUser.passwordA) newErrors.passwordA = "Password is required"
+        else if( newUser.passwordA.length < 8 || newUser.passwordA.length > 64){
+            newErrors.passwordA = "Password must be between 8 and 64 characters long"
+        }
+        else if(!regexPassword.test(newUser.passwordA)){
+            newErrors.passwordA = "Password must include: uppercase, lowercase, number, and special character.."
+        }
+
+
+        if (!newUser.passwordB) newErrors.passwordB = "Capi needs you to repeat your password"
+        else if( newUser.passwordA !== newUser.passwordB){
+            newErrors.passwordB = "Capi needs you to repeat the same password"
+        }
         setErrors(newErrors);
         return Object.values(newErrors).every((error) => error === '');
     };
@@ -119,103 +158,95 @@ export const Register = () => {
         setNewUser({ name: '', lastname: '', email: '', passwordA: '', passwordB: '' });
     };
 
-    return (
-        <>
-            {loading ? <Loading /> : null}
-            <div>
-                <img src="/orange_wave_desktop.png" className="topWave" alt="" />
-                <div className="formNavigate" onClick={() => navigate('/')}>
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-                        <path fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5" d="m14 7l-5 5l5 5" className="beige" />
-                    </svg>
-                    <h4 className="beige">Back to home</h4>
-                </div>
-                <form className='registerLoginForm' onSubmit={handleSubmit}>
-                    <div className='formHeader'>
-                        <img src="/capi_hat.svg" alt="" />
-                        <h4>Create your Account</h4>
-                    </div>
-
-                    <div>
-                        <label htmlFor='name'>Name</label>
-                        <input
-                            type="text"
-                            placeholder="What's your name?"
-                            name='name'
-                            id='name'
-                            value={newUser.name}
-                            onChange={(e) =>
-                                setNewUser({ ...newUser, name: e.target.value })
-                            } />
-                        {errors.name && <p className="error">{errors.name}</p>}
-                    </div>
-
-                    <div>
-                        <label htmlFor='lastname'>Last Name</label>
-                        <input
-                            type="text"
-                            placeholder="What's your last name?"
-                            name='lastname'
-                            id='lastname'
-                            value={newUser.lastname}
-                            onChange={(e) =>
-                                setNewUser({ ...newUser, lastname: e.target.value })
-                            } />
-                        {errors.lastname && <p className="error">{errors.lastname}</p>}
-                    </div>
-
-                    <div>
-                        <label htmlFor='email'>Email</label>
-                        <input
-                            type="email"
-                            placeholder="What's your email?"
-                            name='email'
-                            id='email'
-                            value={newUser.email}
-                            onChange={(e) =>
-                                setNewUser({ ...newUser, email: e.target.value })
-                            } />
-                        {errors.email && <p className="error">{errors.email}</p>}
-                    </div>
-
-                    <div>
-                        <label htmlFor='passwordA'>Password</label>
-                        <input
-                            type="password"
-                            placeholder="Create a password"
-                            name='passwordA'
-                            id='passwordA'
-                            value={newUser.passwordA}
-                            onChange={(e) =>
-                                setNewUser({ ...newUser, passwordA: e.target.value })
-                            } />
-                        {errors.passwordA && <p className="error">{errors.passwordA}</p>}
-                    </div>
-
-                    <div>
-                        <label htmlFor='passwordB'>Repeat Password</label>
-                        <input
-                            type="password"
-                            placeholder="Repeat your password"
-                            name='passwordB'
-                            id='passwordB'
-                            value={newUser.passwordB}
-                            onChange={(e) =>
-                                setNewUser({ ...newUser, passwordB: e.target.value })
-                            } />
-                        {errors.passwordB && <p className="error">{errors.passwordB}</p>}
-                    </div>
-
-                    <PrimaryButton type="submit">Create Account</PrimaryButton>
-                    <Link to={"/login"}>Log in</Link>
-                </form>
-                {emailSent && (
-                    <div>
-                        <p>An email has been sent to your inbox. Please check it.</p>
-                    </div>
-                )}
-                <img src="/capi_photo.svg" className="buttonImg" alt="" />
+    
+  return (
+    <>
+    {loading ? <Loading/> : null}
+    <div>
+        <img src="/orange_wave_desktop.png" className='topWave' alt="" />
+        <div className='formNavigate' onClick={()=>navigate('/')}>
+                <svg xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 24 24"><path fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5" d="m14 7l-5 5l5 5" className='beige'/></svg>
+                <h4 className='beige'>Back to home</h4>
             </div>
-        </>
-    );
-};
+        <form className='registerLoginForm' onSubmit={handleSubmit}>
+            <div className='formHeader'>
+                <img src="/capi_hat.svg" alt="" />
+                <h4>Create your Account</h4>
+            </div>
+
+            <div>
+                <label htmlFor='name'>Name</label>
+                <input 
+                    type="text"
+                    placeholder="Whats's your name?"
+                    name='name'
+                    id='name' 
+                    value={newUser.name}
+                    onChange={(e) =>
+                        setNewUser({...newUser, name: e.target.value})
+                    }/>
+                {errors.name && <p className="error">{errors.name}</p>}
+            </div>
+            <div>
+                <label htmlFor='lastname'>Last name</label>
+                <input 
+                    type="text"
+                    placeholder="And your last name?"
+                    name='lastname'
+                    id='lastname' 
+                    value={newUser.lastname}
+                    onChange={(e) =>
+                        setNewUser({...newUser, lastname: e.target.value})
+                    }/>
+                {errors.lastname && <p className="error">{errors.lastname}</p>}
+            </div>
+            <div>
+                <label htmlFor='email'>Email</label>
+                <input 
+                    type="email"
+                    placeholder="Enter your email"
+                    name='email'
+                    id='email'
+                    value={newUser.email}
+                    onChange={(e) =>
+                        setNewUser({...newUser, email: e.target.value})
+                    }/>
+                {errors.email && <p className="error">{errors.email}</p>}
+            </div>
+            <div>
+                <label htmlFor='password'>Password</label>
+                <input 
+                    type="password"
+                    placeholder="Set your password"
+                    name='password'
+                    id='password'
+                    value={newUser.passwordA}
+                    onChange={(e) =>
+                        setNewUser({...newUser, passwordA: e.target.value})
+                    }/>
+                {errors.passwordA && <p className="error">{errors.passwordA}</p>}
+            </div>
+            <div>
+                <label htmlFor='rePassword'>Repeat password</label>
+                <input 
+                    type="password"
+                    placeholder="Repeat your password"
+                    name='repassword'
+                    id='rePassword' 
+                    value={newUser.passwordB}
+                    onChange={(e) =>
+                        setNewUser({...newUser, passwordB: e.target.value})
+                    }/>
+                {errors.passwordB && <p className="error">{errors.passwordB}</p>}
+            </div>
+
+            <PrimaryButton type="submit">Create Account</PrimaryButton>
+            <Link to={"/login"}>Log in</Link>
+        </form>
+        <img src="/capi_photo.svg"  className='buttonImg' alt="" />
+
+    </div>
+    </>
+
+  )
+}
