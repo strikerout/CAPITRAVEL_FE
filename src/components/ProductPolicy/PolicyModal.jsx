89781a20fd9dc2409f9c07e82cfa411b507e9b4b
Luis Soto <luis13826@gmail.com>
2024-11-26 22:59:19 -0300

    npm run build a ver

--- src/components/ProductPolicy/PolicyModal.jsx
+++ src/components/ProductPolicy/PolicyModal.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import styles from "./PolicyModal.module.scss";
 import CloseButton from "../Buttons/CloseButton";
 import SecundaryButton from "../Buttons/SecundaryButton";
