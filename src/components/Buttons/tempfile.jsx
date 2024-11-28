1c45c107eb9af908066378b5208989d799676f2d
Luis Soto <luis13826@gmail.com>
2024-11-28 05:18:01 -0300

    ..

--- src/components/Buttons/tempfile.jsx
+++
@@ -1,9 +0,0 @@
-import style from "./button.module.scss"
-
-const SecundaryButton = ({ children, func, type = "button" }) => {
-    return (
-        <button className={style.secundaryButton} onClick={func} type={type}> {children} </button>
-    )
-}
-
-export default SecundaryButton
