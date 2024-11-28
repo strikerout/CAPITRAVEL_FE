be272eb9458e19eb329f9ec9842d6c4dd4cbc1ac
Daniel Chacon <dani3l.uebp@gmail.com>
2024-11-28 13:34:54 -0300

    git status

--- src/pages/Reservations/Reservations.jsx
+++ src/pages/Reservations/Reservations.jsx
@@ -9,7 +9,7 @@ import useExperiences from '../../hooks/useExperience';
 
 const Reservations = () => {
   const { reservations, loading, error, fetchReservationsByUser, removeReservation } = useReservations();
-  const {isAlredyReviewed} = useExperiences()
+  const { isAlredyReviewed } = useExperiences();
   const { username } = useAuthLogin();
   const navigate = useNavigate();
   const [reviewStatus, setReviewStatus] = useState({});
@@ -25,33 +25,31 @@ const Reservations = () => {
         const reviewed = await isAlredyReviewed(reservation.experience.id, username);
         statuses[reservation.experience.id] = reviewed;
       }
+      console.log(statuses);
+      
       setReviewStatus(statuses);
     };
 
     if (reservations.length > 0) {
       checkReviewStatuses();
     }
-  }, []);
+    console.log();
+    
+  }, [reservations]);
 
   const getReservationStatus = (checkIn, checkOut) => {
     const today = new Date();
     const checkInDate = new Date(checkIn);
     const checkOutDate = new Date(checkOut);
-  
+
     if (checkOutDate < today) {
-      return 'Past'; 
+      return 'Past';
     } else if (checkInDate > today) {
       return 'Upcoming';
     } else if (checkInDate <= today && checkOutDate >= today) {
       return 'Currently';
     }
   };
-
-  const isAvailableToReview = async (experienceId) => {
-    const reviewed = await isAlredyReviewed(experienceId, username)
-    return reviewed
-  }
-  
 
   const handleRemoveReservation = (id) => {
     Swal.fire({
@@ -166,7 +164,7 @@ const Reservations = () => {
                 <p>{reservation.id}</p>
                 <a href={`/product/${reservation.experience.id}`} className={style.experienceLink}>
                   <span className={style.experienceText}>{reservation.experience.title}</span>
-                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960" width="18px" fill="#5f6368" className={style.icon}>
+                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
                     <path d="M440-280H280q-83 0-141.5-58.5T80-480q0-83 58.5-141.5T280-680h160v80H280q-50 0-85 35t-35 85q0 50 35 85t85 35h160v80ZM320-440v-80h320v80H320Zm200 160v-80h160q50 0 85-35t35-85q0-50-35-85t-85-35H520v-80h160q83 0 141.5 58.5T880-480q0 83-58.5 141.5T680-280H520Z" />
                   </svg>
                 </a>
@@ -174,13 +172,15 @@ const Reservations = () => {
                 <p>{new Date(reservation.checkIn).toLocaleString()}</p>
                 <p>{new Date(reservation.checkOut).toLocaleString()}</p>
                 <p>{`${reservation.user.name} ${reservation.user.lastName}`}</p>
-                <p className={
-                  getReservationStatus(reservation.checkIn, reservation.checkOut) === 'Past'
-                    ? style.past
-                    : getReservationStatus(reservation.checkIn, reservation.checkOut) === 'Upcoming'
+                <p
+                  className={
+                    getReservationStatus(reservation.checkIn, reservation.checkOut) === 'Past'
+                      ? style.past
+                      : getReservationStatus(reservation.checkIn, reservation.checkOut) === 'Upcoming'
                       ? style.upcoming
                       : style.today
-                }>
+                  }
+                >
                   {getReservationStatus(reservation.checkIn, reservation.checkOut)}
                 </p>
 
@@ -194,10 +194,11 @@ const Reservations = () => {
                   >
                     <path
                       fill="#EB5436"
-                      d="M5 3h2a1 1 0 0 0-2 0M4 3a2 2 0 1 1 4 0h2.5a.5.5 0 0 1 0 1h-.441l-.443 5.17A2 2 0 0 1 7.623 11H4.377a2 2 0 0 1-1.993-1.83L1.941 4H1.5a.5.5 0 0 1 0-1zm3.5 3a.5.5 0 0 0-1 0v2a.5.5 0 0 0 1 0zM5 5.5a.5.5 0 0 0-.5.5v2a.5.5 0 0 0 1 0V6a.5.5 0 0 0-.5-.5"
+                      d="M5 3h2a1 1 0 0 0-2 0M4 3a2 2 0 1 1 4 0h2.5a.5.5 0 0 1 0 1h-.441l-.443 5.17A2 2 0 0 1 7.623 11H4.377a2 2 0 0 1-1.993-1.83L1.941 4H1.5a.5.5 0 0 1 0-1H4Z"
                     />
                   </svg>
                 </div>
+                <div>
                 {
                   getReservationStatus(reservation.checkIn, reservation.checkOut) !== 'Past' ? (
                     <p>Evaluate until finished</p> 
@@ -207,8 +208,8 @@ const Reservations = () => {
                     <p>Reviewed</p> 
                   )
                 }
+                </div>
               </li>
-
             ))}
           </ul>
         </div>
