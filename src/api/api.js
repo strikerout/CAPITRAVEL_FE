40f3018561268a6ded38c2283bff60f808cbc21a
Luis Soto <luis13826@gmail.com>
2024-11-30 20:40:26 -0300

    prueba con conexion al back sin variable de entorno

--- src/api/api.js
+++ src/api/api.js
@@ -1,8 +1,16 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_BACKEND_URL, 
+  baseURL: 'https://capitravelbackcopy-production.up.railway.app', 
   timeout: 20000,
 });
 
 export default api;
+
+api.get('/experiences')
+  .then(response => {
+    console.log('Respuesta de la API:', response.data);  // Verifica los datos
+  })
+  .catch(error => {
+    console.error('Error al obtener datos:', error);
+  });
