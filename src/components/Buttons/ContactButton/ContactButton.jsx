0231a2c48c9dd656597f5ffe0d231a2634e8d789
LinaAlvarado <linamariaalvaradopallares@gmail.com>
2024-12-04 09:48:38 -0500

    fix number

--- src/components/Buttons/ContactButton/ContactButton.jsx
+++ src/components/Buttons/ContactButton/ContactButton.jsx
@@ -5,7 +5,7 @@ import style from "./contactButton.module.scss"
 const ContactButton = () => {
   return (
     <a
-      href="https://wa.me/1234567890" 
+      href="https://wa.me/3133776700" 
       target="_blank"
       rel="noopener noreferrer"
       className={style.contactButton}
