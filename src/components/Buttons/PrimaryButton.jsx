d9b41f0c5e1f0b3725a6fc86db6f7dee91de2592
= <=>
2024-12-05 02:54:28 -0300

    Added validation for edit Property

--- src/components/Buttons/PrimaryButton.jsx
+++ src/components/Buttons/PrimaryButton.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import style from './button.module.scss'
 
-const PrimaryButton = ({children, func, type = "button"}) => {
+const PrimaryButton = ({children, func, type = "button", disabled = ""}) => {
   return (
-    <button className={style.primaryButton} onClick={func} type={type}>{children}</button>
+    <button className={style.primaryButton} onClick={func} type={type} disabled={disabled}>{children}</button>
   )
 }
 
